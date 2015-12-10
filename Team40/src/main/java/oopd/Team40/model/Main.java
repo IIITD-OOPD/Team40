d4c1f12cd9b740045f547f85c0e57e91b4fac6ac
priyanka15047 <priyanka15047@iiitd.ac.in>
2015-12-10 21:39:49 +0530

    .

---
+++ Team40/src/main/java/oopd/Team40/model/Main.java
@@ -0,0 +1,14 @@
+package oopd.Team40.model;
+
+public class Main {
+
+/*	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//CircuitHandler.readXMLFile("HalfAdder.xml");
+		Circuit circuit=new Circuit();
+		circuit.readXMLFile("FullAdder.xml");
+
+	}*/
+
+}
